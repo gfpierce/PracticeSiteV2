8843b46c4c3946ef2e7d7a11918be4c2c7d71264
gfpierce <31076033+gfpierce@users.noreply.github.com>
2017-09-15 21:55:35 -0400

    Sticky Nav
    - Added sticky nav element
    - Styled sticky nav
    - Scripted sticky nav

---
+++ resources/js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+
+    /* Sticky navigation */
+    $('.js--section-about').waypoint(function(direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+        offset: '40px;'
+    });
+
+    /* Smooth scrolling */
+    $(function() {
+        $('a[href*="#"]:not([href="#"])').click(function() {
+          if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+              $('html, body').animate({
+                scrollTop: target.offset().top
+              }, 1000);
+              return false;
+            }
+          }
+        });
+    });
+    
+});
