143b147ddbb240a2f1f2e0b6b4b7b10ec0ce40df
gfpierce <31076033+gfpierce@users.noreply.github.com>
2017-09-15 22:17:03 -0400

    Responsive Nav
    - Added responsive nav element
    - Styled responsive nav element
    - Scripted responsive nav element

--- resources/js/script.js
+++ resources/js/script.js
@@ -26,5 +26,41 @@ $(document).ready(function() {
           }
         });
     });
-    
+
+    /* Mobile Navigation */
+    $('.js--nav-icon, .js--main-nav a').click(function(element) {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+
+        if (icon.hasClass('ion-navicon-round)'))
+            return;
+
+        if($(window).width() < 768) {
+            nav.slideToggle(200);
+        }
+
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }
+    });
+
+    $(window).resize(function(){
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+
+        if ($(window).width() > 767){
+            nav.css("display", "block");
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            nav.css("display", "none");
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }
+    });
+
 });
